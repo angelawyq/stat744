e2f34d4a4a22f5e4befb8166510c5761838a752b
Ben Bolker <bbolker@gmail.com>
2018-02-21 12:28:04 -0500

    comments on hw4

--- hw4.R
+++ hw4.R
@@ -7,10 +7,25 @@ library(readxl)
 #library(readr)
 #From last assignment, I learned that tidyverse will automatically load the above three
 theme_set(theme_bw())
-concentration<-read_excel("Concentrations.xlsx")
 
-(concentration %>% mutate(conc.avg=rowMeans(data.frame(Concentration1,Concentration2,Concentration3)))
+## BMB: code styles vary but most people prefer spaces around the assignment
+## operator
+concentration <- read_excel("Concentrations.xlsx")
+## BMB: if you have a choice you should almost always store data as
+##   CSV rather than XLSX
+##   how did you combine the drug category and drug concentration data?
+##   did you do it by hand? (you should almost always figure out a way
+##    to do it programmatically)
+
+(concentration %>%
+ mutate(conc.avg=rowMeans(data.frame(Concentration1,Concentration2,Concentration3)))
 )-> concentration_avg
+
+## BMB: alternately (more "tidily") you could gather() the data into
+## long format, then group_by(Compound) and summarise().  But your approach
+## also works (it might be a bit slower, but that's irrelevant with a small
+## data set like this)
+
 #Here I have calculated the average of the three measures for each compound in one site
 #View(concentration_avg)
 gg1 <- (ggplot(concentration_avg,aes(x=Site, y=conc.avg+1, colour=Compound)) + 
@@ -34,3 +49,8 @@ print(gg1+theme(legend.position="none"))
 #But I think the graph does a better job of getting the message across to reader that there is a general
 #trend that the concentrations of the compounds are much higher at the wastewater-exposed sites.
                         
+## BMB:
+##   spacing between panels?
+##   spacing of x-axis labels?
+##   order of categories (looks alphabetical)?
+##   some labels run off the right-hand edge of the panels ...
